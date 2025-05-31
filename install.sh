778f63ac557ee796e0871f2d5d996ccc62d4449b
FRHermos <fabrialex2021@gmail.com>
2025-05-31 18:00:12 -0500

    0.0.3

---
+++ install.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+# List of packages
+applications=(
+  firefox  # Browser
+  nvim     # Text editor
+  obsidian # Note taking app
+  steam
+  zoom
+  github-desktop-bin
+  telegram-desktop
+  discord
+)
+
+default_applications=(
+  nautilus             # File manager
+  eog                  # Image viewer
+  evince               # Document viewer
+  decibels             # Media player
+  baobab               # Disk analyzer
+  gnome-software       # Browse apps
+  blueberry            # Bluetooth control
+  nm-connection-editor # WiFi control
+  pavucontrol          # Audio control
+  kooha                # Video record
+  ffmpeg               # Convertor
+  htop                 # Process viewer
+)
+
+additional_packages=(
+  xwaylandvideobridge   # Screen sharing
+  sof-firmware          # Sound for Intel latest CPUs
+  cups                  # Printers
+  pipewire              # Sound server
+  power-profiles-daemon # Power profiles switcher                # Makes moving in terminal much easier
+  yay                   # AUR
+  python-pywal16        # Color generator
+)
+
+packages=(
+  hyprland                    # WM
+  ly                          # LM
+  kitty                       # Terminal
+  waybar                      # Status waybar
+  tofi                        # App launcher
+  waypaper                    # GUI Wallpaper manager
+  hyprpaper                   # Wallpaper backend
+  swaync                      # Notifications
+  wl-clipboard                # Clipboard manager
+  hyprlock                    # Lock manager
+  hypridle                    # Idle manager
+  wlogout                     # Logout menu
+  nwg-look                    # GTK Themer
+  udiskie                     # Removable media
+  hyprsunset                  # Blue light filter
+  hyprpicker                  # Color picker
+  hyprpolkitagent             # Authentication daemon
+  xdg-desktop-portal-hyprland # Screen sharing Hyprland
+)
+
+fonts=(
+  ttf-jetbrains-mono     # System font
+  noto-fonts-emoji       # Emoji font
+  awesome-terminal-fonts # Better align icons
+)
+
+themes=(
+  bibata-cursor-theme # Cursor theme
+  adw-gtk-theme       # GTK3 theme
+)
+
+terminal_packages=(
+  fastfetch
+)
+# Update the systemc
+sudo pacman -Sy --noconfirm
+
+# Install packages
+for pkg in "${applications[@]}"; do
+  echo "Installing $pkg..."
+  if ! pacman -Qq "$pkg" &>/dev/null; then
+    yay -S --noconfirm --needed "$pkg"
+  fi
+  echo "$pkg is already installed"
+done
+
+for pkg in "${default_applications[@]}"; do
+  echo "Installing $pkg..."
+  if ! pacman -Qq "$pkg" &>/dev/null; then
+    yay -S --noconfirm --needed "$pkg"
+  fi
+  echo "$pkg is already installed"
+done
+
+for pkg in "${additional_packages[@]}"; do
+  echo "Installing $pkg..."
+  if ! pacman -Qq "$pkg" &>/dev/null; then
+    yay -S --noconfirm --needed "$pkg"
+  fi
+  echo "$pkg is already installed"
+done
+
+for pkg in "${packages[@]}"; do
+  echo "Installing $pkg..."
+  if ! pacman -Qq "$pkg" &>/dev/null; then
+    yay -S --noconfirm --needed "$pkg"
+  fi
+  echo "$pkg is already installed"
+done
+
+for pkg in "${fonts[@]}"; do
+  echo "Installing $pkg..."
+  if ! pacman -Qq "$pkg" &>/dev/null; then
+    yay -S --noconfirm --needed "$pkg"
+  fi
+  echo "$pkg is already installed"
+done
+
+for pkg in "${themes[@]}"; do
+  echo "Installing $pkg..."
+  if ! pacman -Qq "$pkg" &>/dev/null; then
+    yay -S --noconfirm --needed "$pkg"
+  fi
+  echo "$pkg is already installed"
+done
+
+for pkg in "${terminal_packages[@]}"; do
+  echo "Installing $pkg..."
+  if ! pacman -Qq "$pkg" &>/dev/null; then
+    yay -S --noconfirm --needed "$pkg"
+  fi
+  echo "$pkg is already installed"
+done
+
+echo "All packages installed."
