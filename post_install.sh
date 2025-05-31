6991e43e77a280c709b05756b209050487fa0ab2
FRHermos <fabrialex2021@gmail.com>
2025-05-31 18:02:40 -0500

    Delete post_install.sh

--- post_install.sh
+++
@@ -1,7 +0,0 @@
-# Define package categories, you should enable multilib /etc/pacman.conf
-pacman_packages=(
-  nvidia-dkms nvidia-utils nvidia-settings mesa flatpak lib32-mesa vulkan-intel lib32-vulkan-intel primus-vk
-)
-
-echo "Installing packages from official repos..."
-sudo pacman -S --noconfirm "${pacman_packages[@]}"
