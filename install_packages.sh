80f22d6e4f6fda3494d82aa365e535b8474ab48d
FRHermos <fabrialex2021@gmail.com>
2025-05-30 20:56:28 -0500

    0.0.2

--- install_packages.sh
+++ install_packages.sh
@@ -7,36 +7,35 @@ echo "Installing essential base-devel and git..."
 sudo pacman -S --needed git base-devel --noconfirm
 
 # Install yay AUR helper
-if ! command -v yay &>/dev/null; then
-  echo "Installing yay..."
-  git clone https://aur.archlinux.org/yay.git
-  cd yay
-  makepkg -si --noconfirm
-  cd ..
-  rm -rf yay
-fi
+#if ! command -v yay &>/dev/null; then
+#  echo "Installing yay..."
+#  git clone https://aur.archlinux.org/yay.git
+#  cd yay
+#  makepkg -si --noconfirm
+#  cd ..
+#  rm -rf yay
+#fi
 
 # Define package categories
-pacman_packages=(
+#pacman_packages=(
 firefox neovim discord telegram-desktop obsidian sof-firmware cups pipewire alsa-utils pulseaudio
 hyprland ly kitty waybar swaync hyprpaper 
 xdg-desktop-portal-hyprland wl-clipboard hyprlock udiskie 
 nwg-look nautilus blueberry 
 pavucontrol nm-connection-editor htop power-profiles-daemon 
-adw-gtk-theme ttf-jetbrains-mono awesome-terminal-fonts 
+adw-gtk-theme ttf-jetbrains-mono awesome-terminal-fonts
 )
 
-aur_packages=(
-github-desktop-bin betterdiscord-installer 
-rofi-lbonn-wayland-git rofi-calc rofi-emoji 
+paru_packages=( 
+rofi-lbonn-wayland-git rofi-emoji 
 xwaylandvideobridge waypaper hyrpsunset wlogout python-pywal16 bibata-cursor-theme ttf-font-awesome 
 )
 
 echo "Installing packages from official repos..."
-sudo pacman -S --noconfirm "${pacman_packages[@]}"
+sudo pacman -S "${pacman_packages[@]}"
 
-echo "Installing AUR packages with yay..."
-yay -S --noconfirm "${aur_packages[@]}"
+echo "Installing AUR packages with paru..."
+paru -S "${paru_packages[@]}"
 
 # Add zoxide init to .bashrc if not already present
 #if ! grep -q 'zoxide init bash' ~/.bashrc; then
